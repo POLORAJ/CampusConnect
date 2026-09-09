a41ad18f313a675cf76023dadfdfaa0a11cfd499
POLORAJ <polorajkhaidem1@gmail.com>
2026-09-09 13:11:31 +0530

    Complete Sprint 8 responsive UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import './styles/global.css'
+import './styles/layout.css'
+import './styles/components.css'
+import './styles/responsive.css'
+
 import './index.css';
 import './assets/styles/global.css';
 
